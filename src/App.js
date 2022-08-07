dacd01fadc5703cd391ae82ecdd8992dc95449bc
Tingwutobong Okonson <tidarkson@gmail.com>
2022-08-07 03:39:56 +0100

    trello board

--- src/App.js
+++ src/App.js
@@ -1,9 +1,83 @@
-import './App.css';
+// import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>This is my react js trello clone</h1>
+    <div className="trello-master">
+      <nav className="navbar app">App Bar</nav>
+      <nav className="navbar board">Board bar</nav>
+      <div className="board-columns">
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="./images/hero-image.jpg"></img>
+              {/* Medicine and surgery */}
+            </li>
+            <li>Second</li>
+            <li>Third</li>
+            <li>Fourth</li>
+            <li>Second</li>
+            <li>Third</li>
+            <li>Fourth</li> 
+            <li>Second</li>
+            <li>Third</li>
+            <li>Fourth</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="./images/hero-image.jpg"></img>
+            </li>
+            <li>Second</li>
+            <li>Third</li>
+            <li>Fourth</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="./images/hero-image.jpg"></img>
+            </li>
+            <li>Second</li>
+            <li>Third</li>
+            <li>Fourth</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="./images/hero-image.jpg" alt=""></img>
+            </li>
+            <li>Second</li>
+            <li>Third</li>
+            <li>Fourth</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="./images/hero-image.jpg"></img>
+            </li>
+            <li>Second</li>
+            <li>Third</li>
+            <li>Fourth</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+      </div>
     </div>
   );
 }
